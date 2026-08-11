020996564d85322d14fba8e0b321bf03fb402163
JP <amglobal99@yahoo.com>
2026-08-11 11:23:36 -0400

    Initial Commit

---
+++ DummyProject/DummyProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DummyProjectApp.swift
+//  DummyProject
+//
+//  Created by amglobal on 8/11/26.
+//
+
+import SwiftUI
+
+@main
+struct DummyProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
